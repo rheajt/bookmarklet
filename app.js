d7ca859b5d6c63ca57c80d780c7cae69f4fa0b5b
Jordan Rhea <rheajt@gmail.com>
2016-04-27 00:28:02 +0300

    restyled the page with a side navbar found on codepen

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path');
 
 //var routes = require('./routes/index');
 
 var app = express();
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
+// view engine setup
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 //app.use('/', routes);
 app.get('/', function(req, res) {
-  res.sendFile(__dirname + '/public/views/index.html');
+  res.render('index');
+});
+
+app.get('/:pagePath', function(req, res) {
+  res.render(req.params.pagePath);
 });
 
 app.listen(8080, function() {
